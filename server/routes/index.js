cae1db6f75b6c0720bc1b05a94c627972203515d
Nicholas Clawson <nickclaw@gmail.com>
2015-04-12 11:02:50 -0700

    Update index.ejs to load models

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,5 +11,7 @@ router
     .use('/auth', require('./auth/'))
     .use('/admin', require('./admin/'))
     .get('*', function(req, res, next){
-        res.render('__dirname' + "/../../views/index.ejs");
+        res.render('__dirname' + "/../../views/index.ejs", {
+            user: req.user || null
+        });
     });
