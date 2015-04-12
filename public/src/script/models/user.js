e52c1437166719faead883ce045c9812cf530954
Nicholas Clawson <nickclaw@gmail.com>
2015-04-12 10:51:53 -0700

    Add user model

--- public/src/script/models/user.js
+++ public/src/script/models/user.js
@@ -1,42 +1,42 @@
 angular.module('scaffold')
-.factory(
-	'User',
-	[
+.factory('User', [
 	'$resource',
 	'Job',
-		function($resource, Job){
-			var User = $resource(
-				'/api/user/:_id',
-				{
-					_id:'@_id'
-				},
-				{
-					get: {method: 'GET'},
-					save: {method: 'POST'},
-					delete: {method: 'DELETE'},
-					search: {method: 'GET', url: '/api/user', isArray: 'true'}
-				},
-			)
+	function($resource, Job){
+		var User = $resource(
+			'/api/user/:_id',
+			{
+				_id:'@_id'
+			},
+			{
+				get: {method: 'GET'},
+				save: {method: 'POST'},
+				delete: {method: 'DELETE'},
+				search: {method: 'GET', url: '/api/user', isArray: 'true'}
+			},
+		);
 
-			User.prototype.$getJobs = function(){
-				return Job.search({ owner: this._id }); // GET /api/job?owner=:id
-			}	
+		var Reviews = $resource(
+			'/api/user/:_id/review',
+			{},
+			{
+				get: { method: 'GET' },
+				create: { method: 'POST' }
+			}
+		)
+
+		User.prototype.$getJobs = function(){
+			return Job.search({ owner: this._id }); // GET /api/job?owner=:id
 		}
-	]
-)
 
-// GET /api/user - search
-// POST /api/user - create
-// GET /api/user/:id - get
-// POST /api/user/:id - update
-// DELETE /api/user/:id - delete
+		User.prototype.$getReviews = function() {
+			return Reviews.get(this);
+		}
 
-var users = User.search();
+		User.prototype.$review = function(review) {
+			return Reviews.create(review);
+		}
 
-users.$promise.then(function() {
-
-
-	var user = users[0];
-
-	var jobs = user.$getJobs();
-});
+		return User;
+	}
+]);
