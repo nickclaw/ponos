f989dca9ea3417f6c5053e8ac75f34f0afd333eb
Nicholas Clawson <nickclaw@gmail.com>
2015-04-12 11:09:26 -0700

    Add job model

---
+++ public/src/script/models/job.js
@@ -0,0 +1,36 @@
+angular.module('scaffold').factory([
+    '$resource',
+    'Application',
+    function($resource, Application) {
+
+        var job = $resource(
+            '/api/job/:_id',
+            {
+                _id: '@_id'
+            },
+            {
+                get: { method: 'GET' },
+                save: { method: 'POST' },
+                delete: { method: 'DELETE' },
+                create: { method: 'POST', url: '/api/job' },
+                search: { method: 'GET', url: '/api/job', isArray: true }
+            }
+        );
+
+        job.prototype.$getApplications = function(options) {
+            return Application.search({
+                job: this._id
+            }, options);
+        };
+
+        job.prototype.$apply = function(data) {
+            return Application.create(data);
+        };
+
+        job.prototype.$withdraw = function() {
+            return Application.delete(data)
+        };
+
+        return job;
+    }
+]);
