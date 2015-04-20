55dcc74e632806f0730a53f5f98f600e420327c1
Nicholas Clawson <nickclaw@gmail.com>
2015-04-19 22:44:55 -0700

    Add dashboard view

--- public/src/script/routes/index.js
+++ public/src/script/routes/index.js
@@ -12,7 +12,8 @@ angular.module('scaffold')
 
 .controller('IndexController', [
     '$scope',
-    function($scope) {
+    '$http',
+    function($scope, $http) {
 
         //
         // Setup scope
@@ -21,8 +22,12 @@ angular.module('scaffold')
             email: "",
             password: ""
         };
+        $scope.d = {selectedIndex: 0};
         $scope.signup = signup;
+        $scope.results = [];
+        // $scope.profile gets inherited from $rootScope
 
+        $scope.$watch('d.selectedIndex', onTabChange);
 
         //
         // Functions
@@ -32,6 +37,33 @@ angular.module('scaffold')
             $scope.profile.$signup($scope.auth).catch(function(err) {
                 $scope.error = err;
             })
+        }
+
+        function onTabChange(index, lastIndex) {
+            var profile = $scope.profile,
+                role = profile.role;
+
+            if (!role) return;
+
+            $scope.results = [];
+            $http.get(indexes[role][index])
+                .then(function(results) {
+                    $scope.results = results;
+                });
+        }
+
+        // how to get tab content
+        var indexes = {
+            worker: [
+                '/api/user/me/my',
+                '/api/user/me/pending',
+                '/api/user/me/review'
+            ],
+            employer: [
+                '/api/user/me/filled',
+                '/api/user/me/open',
+                '/api/user/me/review'
+            ]
         }
     }
 ]);
--- public/src/script/routes/review-user.js
+++ public/src/script/routes/review-user.js
@@ -47,7 +47,7 @@ angular.module('scaffold')
         }
 
         function cancel() {
-            $location.url('/'):
+            $location.url('/');
         }
     }
 ]);
