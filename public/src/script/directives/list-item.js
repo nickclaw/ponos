655549545d51a574b259b8493ec58d38f3af8415
Nicholas Clawson <nickclaw@gmail.com>
2015-04-26 11:18:33 -0700

    list-item changes

--- public/src/script/directives/list-item.js
+++ public/src/script/directives/list-item.js
@@ -1,18 +1,11 @@
-angular.module('scaffold')
-	.directive(
-		'listItem',
-		[
-		function(){
-
-			return {
-				restrict:'E',
-				replace:'true',
-				transclude:'true',
-				scope:'true',
-				templateUrl:'/static/template/directive/list-item.html',
-				link: function ($scope) {
-					console.log($scope);
-				}
-			}
+angular.module('scaffold').directive('listItem', [
+	function(){
+		return {
+			restrict:'E',
+			replace:'true',
+			transclude:'true',
+			scope:'true',
+			templateUrl:'/static/template/directive/list-item.html'
 		}
-	]);
+	}
+]);
