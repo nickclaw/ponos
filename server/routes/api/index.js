d874056abfe7103915430c403232225e7800e55b
Nicholas Clawson <nickclaw@gmail.com>
2015-04-27 19:21:16 -0700

    Add upload route

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,6 +3,7 @@ var router = require('express').Router();
 router
     .use('/user/', require('./user'))
     .use('/job/', require('./job'))
+    .use('/upload', require('./upload'))
 
     //
     // Error handling
---
+++ server/routes/api/upload.js
@@ -0,0 +1,98 @@
+var router = require('express').Router(),
+    util = require('./util'),
+    vlad = require('vlad'),
+    path = require('path'),
+    fs = require('fs'),
+    gm = require('gm'),
+    multi = require('multiparty');
+
+module.exports = router;
+
+router
+    .post('/image',
+        util.auth,
+
+        // get file
+        // taken from https://github.com/nickclaw/mypassword.is/blob/master/server/util.js#L40
+        function(req, res, next) {
+            var form = new multi.Form({
+                autoFiles: true
+            });
+
+            form.parse(req, function(err, fields, files) {
+                if (err) return next(err);
+                if (!files.file || !files.file.length) return next(vlad.FieldValidationError("No file uploaded."));
+
+                req.file = {
+                    file: files.file[0],
+                    remove: function() {
+                        fs.unlink(files.file[0].path, function(){ /* noooop */ });
+                    }
+                };
+
+                next();
+            });
+        },
+
+        // parse file
+        function(req, res, next) {
+            req.file.image = gm(req.file.file.path);
+            req.file.image.identify(function(err, data) {
+                if (err) return next(vlad.FieldValidationError("Invalid image file. " + err.message));
+                req.file.data = data;
+                next();
+            });
+
+        },
+
+        // validate file
+        function(req, res, next) {
+            var data = req.file.data;
+
+            if (data.format !== 'PNG' && data.format !== 'JPEG') {
+                return next(vlad.FieldValidationError("Invalid file type."));
+            }
+
+            // size checks?
+
+            next();
+
+        },
+
+        // convert
+        function(req, res, next) {
+            var image = req.file.image;
+            image
+                .strip()
+                .interlace('Plane')
+                .quality(85)
+                .toBuffer(function(err, buffer) {
+                    if (err) return next(err);
+                    req.file.buffer = buffer;
+
+                    next();
+                });
+        },
+
+        // save file
+        function(req, res, next) {
+            // clear temp file
+            req.file.remove();
+            delete req.file.file;
+            delete req.file.remove;
+
+            var name = Date.now() + '.jpg';
+
+            fs.writeFile(
+                path.join(__dirname, '../../..', C.APP.RES_DIR, 'upload', name),
+                req.file.buffer,
+                function(err) {
+                    if (err) return next(err);
+                    res.send({
+                        file: path.join('/static/upload/', name)
+                    });
+                }
+            );
+        }
+    )
+;
