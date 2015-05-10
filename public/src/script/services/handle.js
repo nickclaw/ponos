dd5c8993c392e4af0f396e94bbe43f74e4006338
Nicholas Clawson <nickclaw@gmail.com>
2015-05-10 16:40:31 -0700

    Add handle service for handling request errors

---
+++ public/src/script/services/handle.js
@@ -0,0 +1,44 @@
+angular.module('scaffold').factory('handle', [
+    '$mdToast',
+    function($mdToast) {
+
+        return function handle(handles, callback) {
+            if (typeof handles === 'function' || handles === undefined) {
+                callback = handles;
+                handles = {};
+            }
+            handles = handles || {};
+            callback = callback || function(reason) {throw reason;};
+            return function(reason) {
+                var handler = handles[reason.status];
+                if (handler) {
+                    if (typeof handler !== 'function') callback(reason);
+                    return handler.apply(this, arguments);
+                } else {
+                    _handle(reason);
+                    callback(reason);
+                }
+            };
+        }
+
+        function _handle(reason) {
+            switch(reason.status) {
+                case 400:
+                    $mdToast.showSimple("Could not do that. Try again.");
+                    break;
+                case 401:
+                    $mdToast.showSimple("You must be logged in.");
+                    break;
+                case 403:
+                    $mdToast.showSimple("Sorry. You're not allowed to do that.");
+                    break;
+                case 404:
+                    $mdToast.showSimple("Resource not found.");
+                    break;
+                case 500:
+                    $mdToast.showSimple("Server error. Try again.");
+                    break;
+            }
+        }
+    }
+]);
