05682d8352a94f0f9b2ee6b67684699a9289c951
Nicholas Clawson <nickclaw@gmail.com>
2015-05-13 12:01:42 -0700

    Close #30: Map loading indicator

--- public/src/script/directives/location-input.js
+++ public/src/script/directives/location-input.js
@@ -14,22 +14,26 @@ angular.module('scaffold').directive('locationInput', [
 
                 $scope.$watch('location.name', debounce(600, function(address) {
                     if (!address) return;
-                    $scope.location.$resolved = false;
+
+                    $scope.location.$loading = true;
+                    $scope.location.$errored = false;
+
                     var thisRequest = currentRequest = $http.get('/api/upload/address?address=' + address)
                         .then(
                             function(res) {
                                 if (thisRequest !== currentRequest) return;
                                 $scope.location.lat = res.data.latitude;
                                 $scope.location.long = res.data.longitude;
-                                $scope.location.error = null;
-                                $scope.location.$resolved = false;
+                                $scope.location.$errored = false;
+                                $scope.location.$loading = false;
                             },
                             function(err) {
                                 if (thisRequest !== currentRequest) return;
                                 $scope.location.$error = err;
                                 $scope.location.lat = 0;
                                 $scope.location.long = 0;
-                                $scope.location.$resolved = false;
+                                $scope.location.$loading = false;
+                                $scope.location.$errored = true;
                             }
                         )
                 }));
